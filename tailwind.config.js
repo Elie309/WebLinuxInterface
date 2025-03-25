68962a0c8ff80e0e90cb680d29bc8b06d0431201
Elie309 <elie.saade432@gmail.com>
2025-03-25 18:29:56 +0200

    Add Tailwind CSS configuration and styles, update .gitignore, and initialize package.json

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./app/Views/**/*.php" ,
+      "./app/Cells/**/*.php", 
+      "./public/scripts/**/*.js",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
